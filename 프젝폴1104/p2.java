fdbfe5757fc4b50743240975cc6f78f96e5eee89
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:10:51 +0900

    Update p2.java

--- 프젝폴1104/p2.java
+++ 프젝폴1104/p2.java
@@ -1,6 +1,6 @@
-package chapter6;
+package project;
 
-class Book{
+class Book {
 	String author, title, buyer;
 
 	public Book(String author, String title, String buyer) {
@@ -11,24 +11,24 @@ class Book{
 	}
 
 	public boolean equals(Object obj) {
-		Book b = (Book)obj;
-		if(author == b.author && title == b.title)
+		Book b = (Book) obj;
+		if (author == b.author && title == b.title)
 			return true;
 		return false;
 	}
-	
+
 	public String toString() {
 		return buyer + "이 구입한 도서: " + author + "의 " + title;
 	}
 }
 
-public class test2 {
+public class p2 {
 	public static void main(String[] args) {
 		Book a = new Book("황기태", "명품자바", "김하진");
 		Book b = new Book("황기태", "명품자바", "하여린");
 		System.out.println(a);
 		System.out.println(b);
-		if(a.equals(b))
+		if (a.equals(b))
 			System.out.println("같은 색");
 		else
 			System.out.println("다른 색");
