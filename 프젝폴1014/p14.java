de9942a37755e92bf97d1fda629360d754fa47c8
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-15 21:01:09 +0900

    Update and rename test14.java to p14.java

--- 프젝폴1014/p14.java
+++ 프젝폴1014/p14.java
@@ -1,19 +1,19 @@
-package week7_homework;
+package project;
 
 import java.util.Scanner;
 
-public abstract class test14 {
-    private test14 next;
+public abstract class p14 {
+    private p14 next;
 
-    public test14() {
+    public p14() {
         next = null;
     }
 
-    public void setNext(test14 obj) {
+    public void setNext(p14 obj) {
         next = obj;
     }
 
-    public test14 getNext() {
+    public p14 getNext() {
         return next;
     }
 
@@ -25,26 +25,26 @@ public abstract class test14 {
     }
 }
 
-class Line extends test14 {
+class Line extends p14 {
     public void draw() {
         System.out.println("Line");
     }
 }
 
-class Rect extends test14 {
+class Rect extends p14 {
     public void draw() {
         System.out.println("Rect");
     }
 }
 
-class Circle extends test14 {
+class Circle extends p14 {
     public void draw() {
         System.out.println("Circle");
     }
 }
 
 class GraphicEditor {
-    private test14 head;
+    private p14 head;
 
     public GraphicEditor() {
         head = null;
@@ -78,11 +78,10 @@ class GraphicEditor {
         sc.close();
     }
 
-    // 삽입 기능: 새로운 도형을 리스트 맨 앞에 추가
     private void insertShape(Scanner sc) {
         System.out.print("Line(1), Rect(2), Circle(3)>>");
         int draw = sc.nextInt();
-        test14 shape = null;
+        p14 shape = null;
 
         if (draw == 1) {
             shape = new Line();
@@ -95,32 +94,31 @@ class GraphicEditor {
             return;
         }
 
-        shape.setNext(head); // 새 도형의 next를 현재 head로 설정
-        head = shape;        // head를 새 도형으로 갱신
+        shape.setNext(head);
+        head = shape;
     }
 
-    // 특정 위치에 있는 도형 삭제
+
     private void deleteShapeAtPosition(Scanner sc) {
         System.out.print("삭제할 도형의 위치>>");
         int position = sc.nextInt();
 
-        if (position < 1) { // 유효하지 않은 위치
+        if (position < 1) {
             System.out.println("삭제할 수 없습니다.");
             return;
         }
 
-        if (head == null) { // 리스트가 비어 있을 때
+        if (head == null) {
             System.out.println("삭제할 수 없습니다.");
             return;
         }
 
-        if (position == 1) { // 첫 번째 위치 삭제
+        if (position == 1) {
             head = head.getNext();
             return;
         }
 
-        // 위치가 2 이상일 때 해당 위치의 이전 노드를 찾아 삭제
-        test14 current = head;
+        p14 current = head;
         for (int i = 1; i < position - 1; i++) {
             if (current.getNext() == null) {
                 System.out.println("삭제할 수 없습니다.");
@@ -129,22 +127,21 @@ class GraphicEditor {
             current = current.getNext();
         }
 
-        // 삭제할 노드가 존재하지 않는 경우
         if (current.getNext() == null) {
             System.out.println("삭제할 수 없습니다.");
         } else {
-            current.setNext(current.getNext().getNext()); // 삭제 노드를 건너뛰기
+            current.setNext(current.getNext().getNext());
         }
     }
 
-    // 모두 보기 기능: 리스트의 모든 도형 출력
+
     private void showAllShapes() {
         if (head == null) {
             System.out.println("출력할 도형이 없습니다.");
             return;
         }
 
-        test14 current = head;
+        p14 current = head;
         while (current != null) {
             current.draw();
             current = current.getNext();
