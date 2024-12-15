0c4d930a873cc5a2f4fbb5d215e62eb4e185d73b
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-15 20:02:13 +0900

    Update p11.java

--- 프젝폴0930/p11.java
+++ 프젝폴0930/p11.java
@@ -23,7 +23,7 @@ class ArrayUtil{
 	}
 }
 
-public class test11 {
+public class p11 {
 	public static void main(String[] args) {
 		int [] array1 = {1, 5, 7, 9};
 		int [] array2 = {3, 6, -1, 100, 77};
