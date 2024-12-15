e0d1664fecde32d6d44c59ced5801c6942a8834e
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:47:39 +0900

    Update OpenChallenge.java

--- c9/OpenChallenge.java
+++ c9/OpenChallenge.java
@@ -1,4 +1,4 @@
-package chapter9;
+package project;
 
 import java.awt.*;
 import javax.swing.*;
@@ -10,7 +10,7 @@ public class OpenChallenge extends JFrame {
 		super("Open Challenge 9");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Container c = getContentPane(); // 컨텐트팬 알아내기
+		Container c = getContentPane();
 		c.add(new NorthPanel(), BorderLayout.NORTH);
 		c.add(new CenterPanel(), BorderLayout.CENTER);
 		c.add(new SouthPanel(), BorderLayout.SOUTH);
@@ -19,39 +19,39 @@ public class OpenChallenge extends JFrame {
 		setVisible(true);
 	}
 	
-	class NorthPanel extends JPanel { // 컨텐트팬의 NORTH 영역 패널
+	class NorthPanel extends JPanel {
 		public NorthPanel() {
-			setBackground(Color.LIGHT_GRAY); // 배경색 설정
-			setLayout(new FlowLayout()); // FlowLayout 배치관리자 지정
+			setBackground(Color.LIGHT_GRAY);
+			setLayout(new FlowLayout());
 			add(new JLabel("단어 조합 게임! 순서대로 단어를 클릭하세요~~ "));
 			add(new JButton("New Text"));
 		}
 	}
 	
-	class SouthPanel extends JPanel { // 컨텐트팬의 NORTH 영역 패널
+	class SouthPanel extends JPanel {
 		public SouthPanel() {
-			setBackground(Color.YELLOW); // 배경색 설정
-			setLayout(new FlowLayout()); // FlowLayout 배치관리자 지정
+			setBackground(Color.YELLOW);
+			setLayout(new FlowLayout());
 			add(new JLabel("이름"));		
 			add(new JTextField(10));
 		}
 	}
 	
-	class CenterPanel extends JPanel {	// 컨텐트팬의 CENTER 영역 패널
+	class CenterPanel extends JPanel {
 		public CenterPanel() {
-			setLayout(null); // 배치관리자 삭제, 절대 위치에 컴포넌트 삽입
+			setLayout(null);
 			setBackground(Color.WHITE);
 			place(this, text);
 		}
-		private void place(Container c, String text) { // text의 문자열을 단어별로 쪼개 컨테이너 c에 분산 배치 
-			String [] words = text.split(" "); // text 문자열을 " "을 기준으로 단어 분리. words [] 단어 배열 생성 
-			for(int i=0; i<words.length; i++) { // 각 단어 대해 루프
-				JLabel label = new JLabel(words[i]); // 단어를 JLabel로 생성
+		private void place(Container c, String text) { 
+			String [] words = text.split(" "); 
+			for(int i=0; i<words.length; i++) {
+				JLabel label = new JLabel(words[i]);
 				int x = (int)(Math.random()*350);
 				int y = (int)(Math.random()*180);
-				label.setLocation(x,y); // 랜덤한 위치에 배치
+				label.setLocation(x,y);
 				label.setSize(50, 20);
-				c.add(label); // 레이블을 CenterPanel에 부착
+				c.add(label);
 			}			
 		}
 
