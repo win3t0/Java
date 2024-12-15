9064dfdf4761c849f1e4e2b426d80bc2f14a7b45
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:34:41 +0900

    Update p4.java

--- 프젝폴1111_2/p4.java
+++ 프젝폴1111_2/p4.java
@@ -1,19 +1,19 @@
-package chapter8;
+package project;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class test4 {
+public class p4 {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("c:\\temp\\phone.txt"))) {
             System.out.println("c:\\temp\\phone.txt을 읽어 출력합니다");
             
             String line;
-            int count = 1; // 라인 번호 초기화
+            int count = 1;
             
             while ((line = br.readLine()) != null) {
-                System.out.println(count++ + ": " + line); // 라인 번호와 내용 출력
+                System.out.println(count++ + ": " + line);
             }
         } catch (IOException e) {
             System.out.println("입출력 오류");
