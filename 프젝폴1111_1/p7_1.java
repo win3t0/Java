38c35f32a43af069c4072524835c5d70ec8a5c79
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:26:08 +0900

    Update p7_1.java

--- 프젝폴1111_1/p7_1.java
+++ 프젝폴1111_1/p7_1.java
@@ -1,12 +1,12 @@
-package chapter7;
+package project;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// 도시와 위치를 쌍으로 저장하는 클래스
+
 class CityLocationPair {
-    String name;    // 도시 이름
-    Location location; // 도시 위치 (위도, 경도)
+    String name;
+    Location location;
 
     public CityLocationPair(String name, Location location) {
         this.name = name;
@@ -14,12 +14,11 @@ class CityLocationPair {
     }
 }
 
-public class test7_1 {
+public class p7_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<CityLocationPair> cityList = new ArrayList<>();
 
-        // 도시, 경도, 위도 4개 입력받기
         System.out.println("도시, 경도, 위도를 입력하라");
         for (int i = 0; i < 4; i++) {
             String name = sc.next();
@@ -28,27 +27,22 @@ public class test7_1 {
             cityList.add(new CityLocationPair(name, new Location(lat, lon)));
         }
 
-        // 구분선
         System.out.println("----------------------------------");
 
-        // 기록된 모든 도시의 경도와 위도 출력
         System.out.println("저장된 도시와 위치:");
         for (CityLocationPair pair : cityList) {
             System.out.printf("%-6s %-6d %-6d%n", pair.name, pair.location.getLat(), pair.location.getLon());
         }
 
-        // 구분선
         System.out.println("----------------------------------");
 
-        // 그만이라는 입력이 들어오기 전까지 입력한 도시 출력
         System.out.println("도시 이름을 입력하라. '그만'을 입력하면 종료한다.");
         while (true) {
-            String input = sc.next(); // 도시 이름 입력
-            if (input.equals("그만")) { // 종료 조건
+            String input = sc.next();
+            if (input.equals("그만")) {
                 break;
             }
 
-            // 입력된 도시 정보 확인 및 출력
             boolean found = false;
             for (CityLocationPair pair : cityList) {
                 if (pair.name.equals(input)) {
@@ -63,7 +57,6 @@ public class test7_1 {
             }
         }
 
-        // 종료 메시지
         System.out.println("프로그램을 종료합니다.");
         sc.close();
     }
