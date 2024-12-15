3e41a90ec7878ade9b55eedf0702a5923e7d46fa
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:21:58 +0900

    Update OpenChallenge.java

--- 프젝폴1111_1/OpenChallenge.java
+++ 프젝폴1111_1/OpenChallenge.java
@@ -1,12 +1,10 @@
-package chapter7;
+package project;
 import java.util.*;
 
-/**
- * Word 클래스는 영어 단어와 그 뜻을 표현합니다.
- */
+
 class Word {
-    private String word;      // 영어 단어
-    private String meaning;   // 단어의 한국어 뜻
+    private String word;
+    private String meaning;
     
     /**
      * Word 클래스의 생성자입니다.
@@ -52,14 +50,13 @@ class Word {
  * WordQuiz 클래스는 영어 단어 뜻 맞추기 퀴즈 게임을 관리합니다.
  */
 public class OpenChallenge {
-    private Vector<Word> words;  // 단어들을 저장하는 벡터
+    private Vector<Word> words;
     
     /**
      * WordQuiz 클래스의 생성자입니다. 미리 정의된 단어들을 벡터에 추가합니다.
      */
     public OpenChallenge() {
         words = new Vector<Word>();
-        // 단어 추가
         words.add(new Word("love", "사랑"));
         words.add(new Word("animal", "동물"));
         words.add(new Word("friend", "친구"));
@@ -89,14 +86,11 @@ public class OpenChallenge {
         System.out.println("\"명품영어\"의 단어 테스트를 시작합니다. -1을 입력하면 종료합니다.");
         System.out.println("현재 17개의 단어가 들어 있습니다.");
         while(true) {
-	        // 정답 단어를 랜덤하게 선택
 	        Word correctAnswer = words.get(random.nextInt(words.size()));
 	        
-	        // 선택지를 저장할 세트 생성 (중복 방지)
 	        Set<Word> choices = new HashSet<>();
 	        choices.add(correctAnswer);
 	        
-	        // 정답이 아닌 단어를 랜덤하게 추가하여 총 4개의 선택지 생성
 	        while (choices.size() < 4) {
 	            Word incorrectWord = words.get(random.nextInt(words.size()));
 	            if (!incorrectWord.equals(correctAnswer)) {
@@ -104,27 +98,27 @@ public class OpenChallenge {
 	            }
 	        }
 	        
-	        // 선택지를 리스트로 변환하고 섞기
+
 	        List<Word> choiceList = new ArrayList<>(choices);
 	        Collections.shuffle(choiceList);
 	        
-	        // 질문 출력
+
 	        System.out.println(correctAnswer.getWord() + "?");
 	        for (int i = 0; i < choiceList.size(); i++) {
 	            System.out.print("("+(i + 1) + ")" + choiceList.get(i).getMeaning()+" ");
 	        }
 	        System.out.print(":>");
 	        
-	        // 사용자 입력 받기
+
 	        int userAnswer = scanner.nextInt();
 	        
-	        // 입력이 -1일 경우 게임 끝내기
+
 	        if(userAnswer == -1) {
 	        	System.out.println("\"명품영어\"를 종료합니다...");
 	        	break;
 	        }
 	        
-	        // 정답 확인
+
 	        if (choiceList.get(userAnswer - 1).getMeaning().equals(correctAnswer.getMeaning())) {
 	            System.out.println("Excellent !!");
 	        } else {
