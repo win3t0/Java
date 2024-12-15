ba27d90f0b4391cca32c5360368f037e1afcbfa8
win3t0 <80086171+win3t0@users.noreply.github.com>
2024-12-16 01:22:57 +0900

    Update p10.java

--- 프젝폴1111_1/p10.java
+++ 프젝폴1111_1/p10.java
@@ -1,14 +1,11 @@
-package chapter7;
+package project;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//1, 한 라인을 입력받아 ArrayList에 저장
-//2, 중복된 것을 없애거나 애초에 저장하지 않는다
-//3, 그리고 모두 출력한다
 
-public class test10 {
+public class p10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(true) {
@@ -18,13 +15,10 @@ public class test10 {
 			if(str.equals("그만"))
 				break;
 			
-			// 문자열 자르기
 			String[] words = str.split(" ");
 			
-			//ArrayList에 넣기
 			ArrayList<String> wordList = new ArrayList<>(Arrays.asList(words));
 			
-			//중복제거작업
 			for (int i = 0; i < wordList.size(); i++) {
 				for (int j = i+1; j < wordList.size(); j++) {
 					if(wordList.get(i).equals(wordList.get(j))) {
@@ -34,7 +28,6 @@ public class test10 {
 				}
 			}
 			
-			//모두 출력
 			for(String w : wordList)
 				System.out.print(w + " ");
 			System.out.println();
